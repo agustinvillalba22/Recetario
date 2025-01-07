9da246c65e211043cf965f5e5af14722f2f849aa
Agustín Villalba <agustinhernanvillalba@gmail.com>
2025-01-06 22:39:35 -0300

    App para crear recetas saludables

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,9 @@ const Header = ({ changeView }) => {
     <header className="d-flex justify-content-between align-items-center px-4 py-2 bg-light shadow">
       {/* Logo */}
         <div className="d-flex align-items-center">
-        <h1 className="h5 mb-0">Recetari<i class="bi bi-egg-fried"></i></h1>
+        <h1 
+        className="h5 mb-0"
+        onClick={() => changeView('Home')}>Recetari<i class="bi bi-egg-fried"></i></h1>
         </div>
 
       {/* Navegación */}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,8 @@ function Home() {
         <img
         style={{maxWidth: '600px', paddingBottom: '30px'}}
         src="../multimedia/hero.png" alt="imagen-hero" />
-        <h1 className="mb-4">Bienvenido a Cocinando tu Receta</h1>
+        <h1 className="mb-4"
+        style={{maxWidth: '500px', textAlign:'center'}}>Bienvenido a Cocinando tu Receta</h1>
         <p className="mb-4"
         style={{ maxWidth: '600px', maxHeight: '300px', textAlign: 'center', justifyContent:'center', alignItems:'center' }}>
         Descubre una manera fácil y divertida de crear tus propias recetas personalizadas. 
@@ -15,9 +16,6 @@ function Home() {
         lo que te ayudará a tomar decisiones más saludables.
         <strong>¡Haz que cada comida se ajuste a tus necesidades y objetivos nutricionales!</strong>
         </p>
-        <button className='btn btn-primary'
-        style={{ minWidth: '600px', maxHeight: '30px' , paddingTop:0  }} 
-        >Crear receta</button>
     </div>
     );
 }
